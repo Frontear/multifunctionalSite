5a0fb666572c0ad957da060aac8724074295c164
Ali Rizvi <alirizvi609@gmail.com>
2017-11-09 11:40:07 +0900

    Cleaning

--- Main/time.js
+++ Main/time.js
@@ -1,5 +1,5 @@
 ﻿var interval = setInterval(time, 1000); //Create a new variable that calls time() every 1000ms
 function time() {
-    var d = new Date(); // Get Date
-    document.getElementById("time").innerHTML = d.toLocaleTimeString(); // Find the element with ID date, and change the text to the time, from date.
+	var d = new Date(); // Get Date
+	document.getElementById("time").innerHTML = d.toLocaleTimeString(); // Find the element with ID date, and change the text to the time, from date.
 }
